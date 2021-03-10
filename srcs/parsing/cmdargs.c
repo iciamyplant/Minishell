e2aa601faaf0e3ba59ea662bd120cb8b2ec1a219
Emmabrdt <emma.bourdit@gmail.com>
2021-03-10 23:17:11 +0100

    cmd ok

--- srcs/parsing/cmdargs.c
+++ srcs/parsing/cmdargs.c
@@ -114,18 +114,6 @@ int		options(char *whole_cmd, t_copy *copy, t_redir *redir)
 	return (1);
 }
 
-void	init_redir_copy(t_copy *copy, t_redir *redir)
-{
-	copy->i = 0;
-	copy->j = -1;
-	copy->cmd = NULL;
-	redir->sstdout = 1;
-	redir->sstderr = 2;
-	redir->sstdin = 1;
-	redir->out1 = NULL;
-	redir->out2 = NULL;
-	redir->in = NULL;
-}
 
 void	print_parsing(char **args, t_redir *redir)
 {
@@ -157,11 +145,8 @@ void	print_parsing(char **args, t_redir *redir)
 	printf("g_status = %d\n", g_status);
 }
 
-int	quoting(char *whole_cmd, t_copy *copy)
+int	cmd_quoting(char *whole_cmd, t_copy *copy, int j)
 {
-	int		j;
-
-	j = -2;
 	while (whole_cmd[copy->i] == '\'' || whole_cmd[copy->i] == '"')
 	{
 		while (whole_cmd[copy->i] == '"')
@@ -182,6 +167,20 @@ int	quoting(char *whole_cmd, t_copy *copy)
 	return (j);
 }
 
+int	cmd_redir_env(char *whole_cmd, t_copy *copy, int j, t_redir *redir)
+{
+	if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
+	{
+		if (whole_cmd[copy->i + 1] == '\\')
+			copy->cmd[++copy->j] = whole_cmd[copy->i];
+		else
+			j = environnement(whole_cmd, copy, 0, 0);
+	}
+	if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<') && whole_cmd[copy->i - 1] != '\\')
+		j = redirection(whole_cmd, copy, redir);
+	return (j);
+}
+
 char	*cmd(char *whole_cmd, t_copy *copy, t_redir *redir)
 {
 	int		j;
@@ -191,24 +190,11 @@ char	*cmd(char *whole_cmd, t_copy *copy, t_redir *redir)
 	while (whole_cmd[copy->i] && whole_cmd[copy->i] != ' ')
 	{
 		j = -2;
-		if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd[copy->i + 1] == '>' && (!copy->cmd[0] || whole_cmd[copy->i - 1] == ' '))
+		if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd[copy->i + 1] == '>' 
+			&& (!copy->cmd[0] || whole_cmd[copy->i - 1] == ' '))
 			copy->i++;
-		j = quoting(whole_cmd, copy);
-		if (j == -1)
-			return (NULL);
-		if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
-		{
-			if (whole_cmd[copy->i + 1] == '\\')
-				copy->cmd[++copy->j] = whole_cmd[copy->i];
-			else
-				j = environnement(whole_cmd, copy, 0, 0);
-		}
-		if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<') && whole_cmd[copy->i - 1] != '\\')
-		{
-			j = redirection(whole_cmd, copy, redir);
-			if (j == -1)
-				return (NULL);
-		}
+		j = cmd_quoting(whole_cmd, copy, j);
+		j = cmd_redir_env(whole_cmd, copy, j, redir);
 		if ((whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] != '\\') && (copy->cmd[0] || (!copy->cmd[0] 
 			&& (whole_cmd[copy->i - 1] == '"' || whole_cmd[copy->i - 1] == '\'') 
 			&& (whole_cmd[copy->i - 2] == '"' || whole_cmd[copy->i - 2] == '\'' || j == 1))))
@@ -241,3 +227,54 @@ char	*parsing(char *whole_cmd, t_copy *copy, t_redir *redir)
 		return (NULL);
 	return (copy->cmd);
 }
+
+
+/*char	*cmd(char *whole_cmd, t_copy *copy, t_redir *redir)
+{
+	int		j;
+
+	while (whole_cmd[copy->i] && whole_cmd[copy->i] == ' ')
+		copy->i++;
+	while (whole_cmd[copy->i] && whole_cmd[copy->i] != ' ')
+	{
+		j = -2;
+		if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd[copy->i + 1] == '>' && (!copy->cmd[0] || whole_cmd[copy->i - 1] == ' '))
+			copy->i++;
+		while (whole_cmd[copy->i] == '\'' || whole_cmd[copy->i] == '"')
+		{
+			while (whole_cmd[copy->i] == '"')
+				if ((j = double_quote(whole_cmd, copy)) == -1)
+					return (NULL);
+			while (whole_cmd[copy->i] == '\'')
+				if ((simple_quote(whole_cmd, copy)) == -1)
+					return (NULL);
+		}
+		if (whole_cmd[copy->i] == '\\')
+			copy->i++;
+		j = quoting(whole_cmd, copy);
+		if (j == -1)
+			return (NULL);
+		if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
+		{
+			if (whole_cmd[copy->i + 1] == '\\')
+				copy->cmd[++copy->j] = whole_cmd[copy->i];
+			else
+				j = environnement(whole_cmd, copy, 0, 0);
+		}
+		if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<') && whole_cmd[copy->i - 1] != '\\')
+		{
+			j = redirection(whole_cmd, copy, redir);
+			if (j == -1)
+				return (NULL);
+		}
+		if ((whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] != '\\') && (copy->cmd[0] || (!copy->cmd[0] 
+			&& (whole_cmd[copy->i - 1] == '"' || whole_cmd[copy->i - 1] == '\'') 
+			&& (whole_cmd[copy->i - 2] == '"' || whole_cmd[copy->i - 2] == '\'' || j == 1))))
+				break;
+		if (copy->i < strlen(whole_cmd) && ((whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] == '\\') || (whole_cmd[copy->i] != '$' && j == -2)))
+			copy->cmd[++copy->j] = whole_cmd[copy->i];
+		copy->i++;
+	}
+	copy->cmd[copy->j + 1] = 0;
+	return (copy->cmd);
+}*/
