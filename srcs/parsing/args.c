822019d5261153e3bb3836fb62a409b60470320c
Emmabrdt <emma.bourdit@gmail.com>
2021-03-11 11:56:24 +0100

    norme args.c

--- srcs/parsing/args.c
+++ srcs/parsing/args.c
@@ -35,6 +35,26 @@ int	args_quoting(char *whole_cmd, t_copy *copy, int j, size_t i)
 	return (j);
 }
 
+int	args_redir_env(char *whole_cmd, t_copy *copy, int j, size_t i, t_redir *redir)
+{
+	if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
+	{
+		if (whole_cmd[copy->i + 1] == '\\')
+			copy->args[i][++copy->j] = whole_cmd[copy->i];
+		else
+			j = environnement(whole_cmd, copy, 1, i);
+	}
+	if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<') && whole_cmd[copy->i - 1] != '\\')
+	{
+		j = redirection(whole_cmd, copy, redir);
+		if (j == -1)
+			return (-1);
+		if (j == -3)
+			return (-3);
+	}
+	return (j);
+}
+
 char	*args(char *whole_cmd, t_copy *copy, size_t i, t_redir *redir)
 {
 	int		j;
@@ -48,20 +68,12 @@ char	*args(char *whole_cmd, t_copy *copy, size_t i, t_redir *redir)
 		if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd[copy->i + 1] == '>' && whole_cmd[copy->i - 1] == ' ')
 			copy->i++;
 		j = args_quoting(whole_cmd, copy, j, i);
-		//if (j == -1)
-		//	return (NULL);
-		/*while (whole_cmd[copy->i] == '\'' || whole_cmd[copy->i] == '"')
-		{
-			while (whole_cmd[copy->i] == '"')
-				if ((j = double_quote_arg(whole_cmd, copy, i)) == -1)
-					return (NULL);
-			while (whole_cmd[copy->i] == '\'')
-				if ((simple_quote_arg(whole_cmd, copy, i)) == -1)
-					return (NULL);
-		}
-		if (whole_cmd[copy->i] == '\\')
-			copy->i++;*/
-		if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
+		j = args_redir_env(whole_cmd, copy, j, i, redir);
+		if (j == -1)
+			return (NULL);
+		if (j == -3)
+			return (copy->args[i]);
+		/*if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
 		{
 			if (whole_cmd[copy->i + 1] == '\\')
 				copy->args[i][++copy->j] = whole_cmd[copy->i];
@@ -75,7 +87,7 @@ char	*args(char *whole_cmd, t_copy *copy, size_t i, t_redir *redir)
 				return (NULL);
 			if (j == -3)
 				return (copy->args[i]);
-		}
+		}*/
 		if ((whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] != '\\') && (copy->args[i][0] || (!copy->args[i][0] 
 			&& (whole_cmd[copy->i - 1] == '"' || whole_cmd[copy->i - 1] == '\'') 
 			&& (whole_cmd[copy->i - 2] == '"' || whole_cmd[copy->i - 2] == '\'' || j == 1))))
