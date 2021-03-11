059b91b4157f44bf463f905dcb206b058ce2436d
Emmabrdt <emma.bourdit@gmail.com>
2021-03-11 20:01:12 +0100

    double quote cleaninggggg

--- srcs/parsing/d_quote.c
+++ srcs/parsing/d_quote.c
@@ -28,7 +28,7 @@ int	d_quote(char *whole_cmd, t_copy *copy, int j)
 	return (-2);
 }
 
-int	d_quote_arg_util(char *whole_cmd, t_copy *copy, int j, size_t i)
+int	quote_util(char *whole_cmd, t_copy *copy, int j, size_t i)
 {
 	if (whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
 	{
@@ -55,7 +55,7 @@ int	d_quote_arg(char *whole_cmd, t_copy *copy, size_t i, int j)
 	while (whole_cmd[copy->i] && whole_cmd[++copy->i] != '"')
 	{
 		j = 0;
-		j = d_quote_arg_util(whole_cmd, copy, j, i);
+		j = quote_util(whole_cmd, copy, j, i);
 		if (j != 1)
 			copy->args[i][++copy->j] = whole_cmd[copy->i];
 	}
@@ -71,22 +71,14 @@ int	d_quote_arg(char *whole_cmd, t_copy *copy, size_t i, int j)
 	return (0);
 }
 
-int	d_quote_redir(char *whole_cmd, t_copy *copy, t_redir *redir, char *str, int std)
+void	quote_util_redir(char *whole_cmd, t_copy *copy, t_redir *redir, char *str, int std)
 {
-	int j;
-	if (copy->i == (strlen(whole_cmd) - 1)) // si le " ouvrant est le dernier caractere de la chaine
-		return (quote_error('"'));
-	if ((whole_cmd[copy->i + 1] == '"' && whole_cmd[copy->i + 2] == ' ') && !str) // cas de : echo bonjour 1>"" pas normal ou de : echo bonjour 1> "hey""" pas normal
-	{
-		str[redir->i] = ' ';
-		str[redir->i + 1] = 0;
-		copy->i = copy->i + 2;
-		return (-1);
-	}
-	while (whole_cmd[copy->i] && whole_cmd[++copy->i] != '"') //++copy->i; //on decale de 1 car on est sur le " ouvrant
+	int		j;
+
+	while (whole_cmd[copy->i] && whole_cmd[++copy->i] != '"')
 	{
 		j = 0;
-		if (whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\') // $ conserve sa signification speciale
+		if (whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
 		{
 			j = environnement_redir(whole_cmd, copy, std, redir);
 			if (j == -2)
@@ -94,22 +86,36 @@ int	d_quote_redir(char *whole_cmd, t_copy *copy, t_redir *redir, char *str, int 
 				copy->i--;
 				j = 1;
 			}
-			if (whole_cmd[copy->i] == '"' && whole_cmd[copy->i - 1] != '\\') // cas de : echo bonjour 1> "$PATHH"
+			if (whole_cmd[copy->i] == '"' && whole_cmd[copy->i - 1] != '\\') //echo bonjour 1> "$PATHH"
 				break;				
 		}
 		if (whole_cmd[copy->i] == '\\')
 		{
-			if (whole_cmd[copy->i + 1] == '$' || whole_cmd[copy->i + 1] == '\\' // si \ suivit de " $ ou \ garde sa signification
+			if (whole_cmd[copy->i + 1] == '$' || whole_cmd[copy->i + 1] == '\\'
 					|| whole_cmd[copy->i + 1] == '"')
 				copy->i++;
 		}
 		if (j != 1)
 			str[++redir->i] = whole_cmd[copy->i];
 	}
-	if ((copy->i == strlen(whole_cmd)) && whole_cmd[copy->i] != '"') // si y a pas de " fermant
+}
+
+int	d_quote_redir(char *whole_cmd, t_copy *copy, t_redir *redir, char *str, int std)
+{
+	if (copy->i == (strlen(whole_cmd) - 1))
+		return (quote_error('"'));
+	if ((whole_cmd[copy->i + 1] == '"' && whole_cmd[copy->i + 2] == ' ') && !str) // echo bonjour 1>"" pas normal ou de : echo bonjour 1> "hey""" pas normal
+	{
+		str[redir->i] = ' ';
+		str[redir->i + 1] = 0;
+		copy->i = copy->i + 2;
+		return (-1);
+	}
+	quote_util_redir(whole_cmd, copy, redir, str, std);
+	if ((copy->i == strlen(whole_cmd)) && whole_cmd[copy->i] != '"')
 		return (quote_error('"'));
 	str[redir->i + 1] = 0;
-	copy->i++; // on decale de 1 car on est sur le " fermant
+	copy->i++;
 	if (whole_cmd[copy->i] != ' ')
 		return (1);
 	return (-1);
