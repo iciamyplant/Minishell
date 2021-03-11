73160e22ee2b09511ec0c4b7096dafd85fa3495d
Emmabrdt <emma.bourdit@gmail.com>
2021-03-11 18:32:13 +0100

    norme args.c

--- srcs/parsing/args.c
+++ srcs/parsing/args.c
@@ -2,6 +2,7 @@
 
 int	init_malloc_args(char *whole_cmd, t_copy *copy, size_t i)
 {
+	copy->j = -1;
 	copy->args[i] = NULL;
 	g_error = 0;
 	copy->args[i] = malloc(sizeof(char) * (strlen(whole_cmd) + 1));
@@ -16,6 +17,9 @@ int	init_malloc_args(char *whole_cmd, t_copy *copy, size_t i)
 
 int	args_quoting(char *whole_cmd, t_copy *copy, int j, size_t i)
 {
+	if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd
+		[copy->i + 1] == '>' && whole_cmd[copy->i - 1] == ' ')
+		copy->i++;
 	while (whole_cmd[copy->i] == '\'' || whole_cmd[copy->i] == '"')
 	{
 		while (whole_cmd[copy->i] == '"')
@@ -30,7 +34,8 @@ int	args_quoting(char *whole_cmd, t_copy *copy, int j, size_t i)
 				return (-1);
 		}
 	}
-	if (whole_cmd[copy->i] == '\\' && copy->i == (int)ft_strlen(whole_cmd) - 1 && whole_cmd[copy->i - 1] != '\\')
+	if (whole_cmd[copy->i] == '\\' && copy->i == (int)ft_strlen(whole_cmd)
+		- 1 && whole_cmd[copy->i - 1] != '\\')
 		return (j);
 	if (whole_cmd[copy->i] == '\\')
 		copy->i++;
@@ -39,40 +44,50 @@ int	args_quoting(char *whole_cmd, t_copy *copy, int j, size_t i)
 
 int	args_redir_env(char *whole_cmd, t_copy *copy, int j, size_t i, t_redir *redir)
 {
-	if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] != '\\')
+	if (whole_cmd[copy->i] && whole_cmd[copy->i] == '$'
+		&& whole_cmd[copy->i - 1] != '\\')
 	{
 		if (whole_cmd[copy->i + 1] == '\\')
 			copy->args[i][++copy->j] = whole_cmd[copy->i];
 		else
 			j = environnement(whole_cmd, copy, 1, i);
 	}
-	if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<') && whole_cmd[copy->i - 1] != '\\')
+	if ((whole_cmd[copy->i] == '>' || whole_cmd[copy->i] == '<')
+		&& whole_cmd[copy->i - 1] != '\\')
 		j = redirection(whole_cmd, copy, redir);
 	return (j);
 }
+
+/*void	copy_arg(char *whole_cmd, t_copy *copy, size_t i, t_redir *redir)
+{
+
+}*/
 
 char	*args(char *whole_cmd, t_copy *copy, size_t i, t_redir *redir)
 {
 	int		j;
 
-	copy->j = -1;
 	if (init_malloc_args(whole_cmd, copy, i) == -1)
 		return (NULL);
-	while (whole_cmd[copy->i] && (whole_cmd[++copy->i] != ' ' || (whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] == '\\')))
+	while (whole_cmd[copy->i] && (whole_cmd[++copy->i] != ' '
+			|| (whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] == '\\')))
 	{
 		j = 0;
-		if ((whole_cmd[copy->i] == '1' || whole_cmd[copy->i] == '2') && whole_cmd[copy->i + 1] == '>' && whole_cmd[copy->i - 1] == ' ')
-			copy->i++;
 		j = args_quoting(whole_cmd, copy, j, i);
 		j = args_redir_env(whole_cmd, copy, j, i, redir);
 		if (j == -3)
 			return (copy->args[i]);
-		if ((whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] != '\\') && (copy->args[i][0] || (!copy->args[i][0] 
-			&& (whole_cmd[copy->i - 1] == '"' || whole_cmd[copy->i - 1] == '\'') 
-			&& (whole_cmd[copy->i - 2] == '"' || whole_cmd[copy->i - 2] == '\'' || j == 1))))
-				break;
-		if (whole_cmd[copy->i] && (whole_cmd[copy->i] != ' ' || (whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] == '\\')) && j != 1 && j!= 4 && ((whole_cmd[copy->i] == '$' && whole_cmd[copy->i - 1] == '\\') || (whole_cmd[copy->i] != '$')))
-			copy->args[i][++copy->j] = whole_cmd[copy->i];	
+		if ((whole_cmd[copy->i] == ' ' && whole_cmd[copy->i - 1] != '\\')
+			&& (copy->args[i][0] || (!copy->args[i][0] && (whole_cmd
+						[copy->i - 1] == '"' || whole_cmd[copy->i - 1] == '\'')
+					&& (whole_cmd[copy->i - 2] == '"' || whole_cmd
+						[copy->i - 2] == '\'' || j == 1))))
+			break ;
+		if (whole_cmd[copy->i] && (whole_cmd[copy->i] != ' ' || (whole_cmd
+					[copy->i] == ' ' && whole_cmd[copy->i - 1] == '\\'))
+			&& j != 1 && j!= 4 && ((whole_cmd[copy->i] == '$'
+					&& whole_cmd[copy->i - 1] == '\\') || (whole_cmd[copy->i] != '$')))
+			copy->args[i][++copy->j] = whole_cmd[copy->i];
 	}
 	copy->args[i][copy->j + 1] = 0;
 	return (copy->args[i]);
